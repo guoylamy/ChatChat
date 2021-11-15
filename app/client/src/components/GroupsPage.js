8a36b19ba4703ed02a07bb6a6a8be707ff38af14
yycTom <860343832@qq.com>
2021-11-15 15:53:46 -0500

    revise grouppage, add global variables

--- app/client/src/components/GroupsPage.js
+++ app/client/src/components/GroupsPage.js
@@ -1,7 +1,10 @@
-import React from "react"
+import React, {useState} from "react"
 import {useParams} from "react-router-dom"
 function GroupsPage() {
     const {username} = useParams()
+    const [topic, setTopic] = useState()
+    const [groupNameToBeCreated, setGroupNameToBeCreated] = useState()
+    const [groupTypeToBeCreated, setGroupTypeToBeCreated] = useState()
     let group_data = {"public_group" : ["Dua Lipa fans group", "Ed sheeran fans group"], "private_group" : ["cis557 project team", "cis521 team"],}
     let topic_data = {"Dua Lipa fans group" : ['pop'], "Ed sheeran fans group" : ["beautiful people"], 
                        "cis557 project team" : ['api'], "cis521 team":['robot']}
@@ -17,12 +20,21 @@ function GroupsPage() {
                     <div>
                         <div>{group_name}</div>
                         <div>topics : {topic_data[group_name]}</div>
-                        <button>Delete</button>
+                        <button onClick={deletePublicGroups(group_name)}>Delete</button>
                     </div>
                     
                 ))}
             </div>
         )
+    }
+
+    function deletePublicGroups(group_name) {
+        // need to connect database to delete group with group_name
+
+    }
+
+    function deletePrivateGroups(group_name) {
+        // need to connect database to delete group with group_name
     }
 
     function getPrivateGroups() {
@@ -36,24 +48,32 @@ function GroupsPage() {
                     <div>
                         <div>{group_name}</div>
                         <div>topics: {topic_data[group_name]}</div>
-                        <button>Delete</button>
+                        <button onClick={deletePrivateGroups(group_name)}>Delete</button>
                     </div>
-                    
                 ))}
             </div>
         )
+    }
+    function createGroupButton() {
+        // pass two parameters: group name and group type. connect database to create a group
+    }
+    function createGroupGroupName(event) {
+        setGroupNameToBeCreated(event.target.value)
+    }
+    function createGroupSelectOption(event) {
+        setGroupTypeToBeCreated(event.target.value)
     }
     function createGroup() {
         return (
             <div> 
                 <h1>Create Your Group</h1>
-                <div>Group Name <input type="text"></input></div>
+                <div>Group Name <input type="text" value={groupNameToBeCreated} onChange={createGroupGroupName}></input></div>
                 <div>Group Type</div>
-                <select>
-                    <option>Private</option>
-                    <option>Public</option>
+                <select onChange={createGroupSelectOption}>
+                    <option value="Private">Private</option>
+                    <option value="Public">Public</option>
                 </select>
-                <button>create</button>
+                <button onClick={createGroupButton}>create</button>
             </div>
         )
     }
@@ -66,6 +86,17 @@ function GroupsPage() {
             </div>
         )
     }
+    function handleFilterChange(event) {
+        setTopic(event.target.value)
+    }
+    function filterByGroup() {
+        return (
+            <div>
+                filter
+                <input type="text" placeholder="topic" value={topic} onChange={handleFilterChange}></input>
+            </div>
+        )
+    }
     return (
 
         <div>
@@ -74,6 +105,7 @@ function GroupsPage() {
             {getPrivateGroups()}
             {createGroup()}
             {joinPublicGroup()}
+            {filterByGroup()}
         </div>
     )
 }
