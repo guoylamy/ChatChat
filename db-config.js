e4316025609de402a9f6c7624d062fe5e1105057
Yilin Guo <gyilin@seas.upenn.edu>
2021-12-20 22:41:22 -0500

    edit port

--- db-config.js
+++ db-config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   host: process.env.host,
-  port: process.env.port,
+  port: process.env.dbport,
   user: process.env.user,
   password: process.env.password,
   database: process.env.database,
