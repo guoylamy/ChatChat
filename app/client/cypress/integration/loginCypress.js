6700648445c8c799437034b24f0d777ebe750294
yycTom <860343832@qq.com>
2021-12-21 23:57:47 -0500

    fix cypress

--- app/client/cypress/integration/loginCypress.js
+++ app/client/cypress/integration/loginCypress.js
@@ -2,9 +2,9 @@ describe('Tests the login page', () => {
   it('Enter Username and passwords successfully', () => {
     cy.visit('http://localhost:8081/');
     cy.get('#NameInput').should('exist');
-    cy.get('#NameInput').type('123').should('have.value', '123');
+    cy.get('#NameInput').type('RoutesTestUser3').should('have.value', 'RoutesTestUser3');
     cy.get('#pwdInput').should('exist');
-    cy.get('#pwdInput').type('123').should('have.value', '123');
+    cy.get('#pwdInput').type('test').should('have.value', 'test');
     cy.get('#sign').should('exist');
     cy.get('#sign').click();
     cy.get('#navbar').should('exist');
